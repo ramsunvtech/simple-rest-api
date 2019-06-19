b600c6674d3a53ae163a96112bede465acfcdd1f
Venkat.R <venkat@uob.sg>
2019-06-19 21:43:15 +0800

    (tech) Simple Rest API

---
+++ src/main/java/com/simple/rest/api/simpleRestApi/SimpleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.simple.rest.api.simpleRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApiApplication.class, args);
+	}
+
+}
