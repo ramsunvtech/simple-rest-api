39f25b75c9df1f20485200a627bad23ee2c64472
Venkat.R <venkat@uob.sg>
2019-06-22 19:05:07 +0800

    Post Call API

--- src/main/java/com/simple/rest/api/simpleRestApi/com/simple/rest/api/simpleRestApi/service/HelloWorldService.java
+++ src/main/java/com/simple/rest/api/simpleRestApi/com/simple/rest/api/simpleRestApi/service/HelloWorldService.java
@@ -1,8 +1,10 @@
 package com.simple.rest.api.simpleRestApi.com.simple.rest.api.simpleRestApi.service;
 
+import com.simple.rest.api.simpleRestApi.com.simple.rest.api.simpleRestApi.service.PostCountry;
 import org.json.JSONObject;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldService {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET,headers="Accept=application/json")
-    public String sayHello()
+    public String getHello()
     {
         JSONObject jsonResponse = new JSONObject();
         jsonResponse.put("requestCode", 32424);
         jsonResponse.put("routingCode", "lax");
+        return jsonResponse.toString();
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
+    public String postHello(@RequestBody PostCountry country)
+    {
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("postCountry", country.getCountry());
+        jsonResponse.put("postRoutingCode", "lax");
         return jsonResponse.toString();
     }
 
---
+++ src/main/java/com/simple/rest/api/simpleRestApi/com/simple/rest/api/simpleRestApi/service/PostCountry.java
@@ -0,0 +1,21 @@
+package com.simple.rest.api.simpleRestApi.com.simple.rest.api.simpleRestApi.service;
+
+public class PostCountry {
+
+
+    public String country;
+
+    public PostCountry() {}
+
+    public PostCountry(String country) {
+        this.country = country;
+    }
+
+    public void setCountry(String country){
+        this.country=country;
+    }
+    public String getCountry() {
+        return country;
+    }
+
+}
